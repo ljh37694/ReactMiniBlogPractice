524acd01c53f254580a9faa853ff6cdf303dec4f
ljh37694 <ljh37694@naver.com>
2023-12-28 21:02:16 +0900

    미니 블로그 실습 테스트

---
+++ src/component/list/CommentList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import CommentListItem from "./CommentListItem";
+
+const Wraaper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+function CommentList(props) {
+    const { comments } = props;
+
+    return (
+        <Wraaper>
+            {comments.map((comment) => {
+                return <CommentListItem key={comment.id} comment={comment} />;
+            })}
+        </Wraaper>
+    );
+}
+
+export default CommentList;
